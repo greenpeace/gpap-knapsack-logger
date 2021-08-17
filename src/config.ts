c0795335a03d05441b29ec98b25d85eb0ad3d76c
KT <tan.kairen@gmail.com>
2021-08-17 21:33:31 +1000

    :bug: decrease max log size

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,11 @@ const config = {
         maxLogSize: {
             /**
              * The max size of a log is 256k, aka 256000 bytes.
+             * @note in practice 256k still sees us creating logs as large as 300kb.
+             * So we are making this more conservative, down to 200k.
              * @see https://cloud.google.com/logging/quotas#log-limits
              */
-            bytes: 256000,
+            bytes: 200000,
         },
     },
     defaultCloudFunctionRegion: 'us-central1',
